f0cf133844d3ebbb446ee87a7593f20ee36a64dd
NiMiR13 <r0932665555@gmail.com>
2026-01-28 21:31:52 +0200

    fix: use double quotes and remove russian comments

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    instances = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
+
+    for person_dict in people:
+        person_instance = Person.people[person_dict["name"]]
+        for partner_type in ["wife", "husband"]:
+            partner_name = person_dict.get(partner_type)
+            if partner_name:
+                setattr(
+                    person_instance,
+                    partner_type,
+                    Person.people[partner_name]
+                )
+
+    return instances
